54c0416f3f5d778a4f833945122aba50780e0c5e
ArnauMunoz <72936139+ArnauMunoz@users.noreply.github.com>
2020-12-03 18:06:37 +0100

    Add files via upload

--- m03uf06review/src/main/java/cat/copernic/m03uf06review/orm/OrmMain.java
+++ m03uf06review/src/main/java/cat/copernic/m03uf06review/orm/OrmMain.java
@@ -5,26 +5,63 @@
  */
 package cat.copernic.m03uf06review.orm;
 
+import cat.copernic.m03uf06review.conexiobdd.ConexioBDD;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 2 entrega
- * 
+ *
  * En aquesta secció cal accedir a una taula de MySQL amb un camp de cada tipus:
- * 
+ *
  * int o long, double o float, boolean, char, String i java.sql.Date
- * 
- * Recòrrer el result set i mostrar-lo per la consola com a instancies de la 
+ *
+ * Recòrrer el result set i mostrar-lo per la consola com a instancies de la
  * classe Registre, que tindrà l'estructura de la teva taula.
- * 
- * 
+ *
+ *
  * @author pep
  */
-public class OrmMain {
+public class OrmMain extends ConexioBDD {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static List<Persona> listaPersonas = new ArrayList();
+
+    public static void fillPersonas() {
+
+        Connection con = getConexioBDD();
+
+        try {
+            String query = "select * from persona";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+
+                Persona p = new Persona(rs.getInt("persona_id"), rs.getString("persona_nom"),
+                        rs.getString("persona_direccio"), rs.getDouble("persona_altura"),
+                        rs.getBoolean("persona_viva"), rs.getDate("persona_naixement"));
+
+                listaPersonas.add(p);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
-    
+
+    public static void showPersones() {
+        for (int i = 0; i < listaPersonas.size(); i++) {
+            System.out.println(listaPersonas.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        fillPersonas();
+        showPersones();
+    }
 }
